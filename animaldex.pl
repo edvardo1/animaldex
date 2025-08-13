0a7cc7ab3a9837e31647a0fc0345a1e06cc34c51
edvardo1 <eduardobeloni@mailfence.com>
2025-08-13 07:17:38 -0300

    adicionar nova linha e consertar nome cientifico do gorila

--- animaldex.pl
+++ animaldex.pl
@@ -21,7 +21,7 @@ diet('Herbívoro', her).
 diet(_, error('dieta inválida')).
 
 %%               nome comum            nome cientifico clas diet altr mass contint ameaca
-animal(           'Gorila', 'Gorilla Gorilla Gorilla', mam, her, 150, 180, africa,      0).
+animal(           'Gorila', '        Gorilla gorilla', mam, her, 150, 180, africa,      0).
 animal('Pinguim-imperador',    'Aptenodytes forsteri',  av, car, 100,  30,    ant,      0).
 
 procura_animal(Nc, Ns, Clas, Diet, H, M, Co, Am) :-
@@ -31,9 +31,9 @@ procura_animal(Nc, Ns, Clas, Diet, H, M, Co, Am) :-
   diet(Di_s, Diet),
   
   write('nome comum: '),      write(Nc),   write('\n'),
-  write('nome cientifico: '), write(Nc),   write('\n'),
+  write('nome cientifico: '), write(Ns),   write('\n'),
   write('classe: '),          write(Cl_s), write('\n'),
   write('dieta: '),           write(Di_s), write('\n'),
   write('altura: '),          write(H),    write('\n'),
   write('continente: '),      write(Co_s), write('\n'),
-  write('ameaca: '),          write(Am).   write('\n').
+  write('ameaca: '),          write(Am),   write('\n').
